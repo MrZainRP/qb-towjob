0ad4b489fa0aa31a63c1bde77fabd4b7aee496f5
Mister Zain <95512163+MrZainRP@users.noreply.github.com>
2022-09-17 00:29:47 +1000

    mz-skills update

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local PaymentTax = 15
+local PaymentTax = Config.Paymenttax
 local Bail = {}
 
 RegisterNetEvent('qb-tow:server:DoBail', function(bool, vehInfo)
@@ -9,21 +9,37 @@ RegisterNetEvent('qb-tow:server:DoBail', function(bool, vehInfo)
         if Player.PlayerData.money.cash >= Config.BailPrice then
             Bail[Player.PlayerData.citizenid] = Config.BailPrice
             Player.Functions.RemoveMoney('cash', Config.BailPrice, "tow-paid-bail")
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_cash", {value = Config.BailPrice}), 'success')
+            if Config.NotifyType == 'qb' then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_cash", {value = Config.BailPrice}), 'success')
+            elseif Config.NotifyType == "okok" then
+                TriggerClientEvent('okokNotify:Alert', source, "PAID WITH CASH", Lang:t("success.paid_with_cash", {value = Config.BailPrice}), 3500, 'success')
+            end
             TriggerClientEvent('qb-tow:client:SpawnVehicle', src, vehInfo)
         elseif Player.PlayerData.money.bank >= Config.BailPrice then
             Bail[Player.PlayerData.citizenid] = Config.BailPrice
             Player.Functions.RemoveMoney('bank', Config.BailPrice, "tow-paid-bail")
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_bank", {value = Config.BailPrice}), 'success')
+            if Config.NotifyType == 'qb' then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("success.paid_with_bank", {value = Config.BailPrice}), 'success')
+            elseif Config.NotifyType == "okok" then
+                TriggerClientEvent('okokNotify:Alert', source, "PAID WITH BANK", Lang:t("success.paid_with_bank", {value = Config.BailPrice}), 3500, 'success')
+            end
             TriggerClientEvent('qb-tow:client:SpawnVehicle', src, vehInfo)
         else
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("error.no_deposit", {value = Config.BailPrice}), 'error')
+            if Config.NotifyType == 'qb' then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("error.no_deposit", {value = Config.BailPrice}), 'error')
+            elseif Config.NotifyType == "okok" then
+                TriggerClientEvent('okokNotify:Alert', source, "NO DEPOSIT", Lang:t("error.no_deposit", {value = Config.BailPrice}), 3500, 'error')
+            end
         end
     else
         if Bail[Player.PlayerData.citizenid] ~= nil then
             Player.Functions.AddMoney('bank', Bail[Player.PlayerData.citizenid], "tow-bail-paid")
             Bail[Player.PlayerData.citizenid] = nil
-            TriggerClientEvent('QBCore:Notify', src, Lang:t("success.refund_to_cash", {value = Config.BailPrice}), 'success')
+            if Config.NotifyType == 'qb' then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("success.refund_to_cash", {value = Config.BailPrice}), 'success')
+            elseif Config.NotifyType == "okok" then
+                TriggerClientEvent('okokNotify:Alert', source, "PAID WITH BANK", Lang:t("success.refund_to_cash", {value = Config.BailPrice}), 3500, 'success')
+            end
         end
     end
 end)
@@ -33,20 +49,17 @@ RegisterNetEvent('qb-tow:server:nano', function(vehNetID)
     local Player = QBCore.Functions.GetPlayer(src)
     local targetVehicle = NetworkGetEntityFromNetworkId(vehNetID)
     if not Player then return end
-
     local playerPed = GetPlayerPed(src)
     local playerVehicle = GetVehiclePedIsIn(playerPed, true)
     local playerVehicleCoords = GetEntityCoords(playerVehicle)
     local targetVehicleCoords = GetEntityCoords(targetVehicle)
     local dist = #(playerVehicleCoords - targetVehicleCoords)
-    if Player.PlayerData.job.name ~= "tow" or dist > 11.0 then
-        return DropPlayer(src, Lang:t("info.skick"))
-    end
-
     local chance = math.random(1,100)
-    if chance < 26 then
-        Player.Functions.AddItem("cryptostick", 1, false)
-        TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items["cryptostick"], "add")
+    if Config.bonus then
+        if chance <= Config.bonuschance then
+            Player.Functions.AddItem(Config.bonusitem, 1, false)
+            TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.bonusitem], "add")
+        end
     end
 end)
 
@@ -54,32 +67,33 @@ RegisterNetEvent('qb-tow:server:11101110', function(drops)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
-
     local playerPed = GetPlayerPed(src)
     local playerCoords = GetEntityCoords(playerPed)
     if Player.PlayerData.job.name ~= "tow" or #(playerCoords - vector3(Config.Locations["main"].coords.x, Config.Locations["main"].coords.y, Config.Locations["main"].coords.z)) > 6.0 then
         return DropPlayer(src, Lang:t("info.skick"))
     end
-
     drops = tonumber(drops)
     local bonus = 0
-    local DropPrice = math.random(150, 170)
+    local DropPrice = math.random(Config.Lowpay, Config.Highpay)
     if drops > 5 then
         bonus = math.ceil((DropPrice / 10) * 5)
     elseif drops > 10 then
-        bonus = math.ceil((DropPrice / 10) * 7)
+        bonus = math.ceil((DropPrice / 10) * 6)
     elseif drops > 15 then
-        bonus = math.ceil((DropPrice / 10) * 10)
+        bonus = math.ceil((DropPrice / 10) * 7)
     elseif drops > 20 then
-        bonus = math.ceil((DropPrice / 10) * 12)
+        bonus = math.ceil((DropPrice / 10) * 8)
     end
     local price = (DropPrice * drops) + bonus
     local taxAmount = math.ceil((price / 100) * PaymentTax)
     local payment = price - taxAmount
-
     Player.Functions.AddJobReputation(1)
     Player.Functions.AddMoney("bank", payment, "tow-salary")
-    TriggerClientEvent('QBCore:Notify', src, Lang:t("success.you_earned", {value = payment}), 'success')
+    if Config.NotifyType == 'qb' then
+        TriggerClientEvent('QBCore:Notify', src, Lang:t("success.you_earned", {value = payment}), 'success')
+    elseif Config.NotifyType == "okok" then
+        TriggerClientEvent('okokNotify:Alert', source, "YOU BEEN PAID", Lang:t("success.you_earned", {value = payment}), 3500, 'success')
+    end
 end)
 
 QBCore.Commands.Add("npc", Lang:t("info.toggle_npc"), {}, false, function(source)
@@ -91,4 +105,60 @@ QBCore.Commands.Add("tow", Lang:t("info.tow"), {}, false, function(source)
     if Player.PlayerData.job.name == "tow"  or Player.PlayerData.job.name == "mechanic" then
         TriggerClientEvent("qb-tow:client:TowVehicle", source)
     end
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel1', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level1Low, Config.Level1High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel2', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level2Low, Config.Level2High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel3', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level3Low, Config.Level3High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel4', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level4Low, Config.Level4High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel5', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level5Low, Config.Level5High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel6', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level6Low, Config.Level6High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel7', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level7Low, Config.Level7High)
+    Player.Functions.AddMoney('cash', Bonus)
+end)
+
+RegisterNetEvent('qb-towjob:server:NPCBonusLevel8', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local Bonus = math.random(Config.Level8Low, Config.Level8High)
+    Player.Functions.AddMoney('cash', Bonus)
 end)
